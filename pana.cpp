856df3ce637c6598ec8ea311641f1fe25351468b
Pablo080 <pablo.vela.castillo@gmail.com>
2022-02-24 10:48:58 +0100

    pana

---
+++ pana.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+int numero1, numero2, suma;
+cout<<"Dime un numero: ";
+cin>> numero1;
+cout<<"Dime otro numero: ";
+cin>>numero2;
+suma = numero1 + numero2;
+std::cout <<"Su suma es" << suma;
+
+    return 0;
+}
