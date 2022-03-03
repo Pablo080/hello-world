30b6714069567447fbdf1e501d525ae6c15fcda0
Pablo080 <pablo.vela.castillo@gmail.com>
2022-03-03 10:52:48 +0100

    Media

---
+++ Media.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+float nota1,nota2,nota3,nota4, media;
+cout << "Dame la primera nota: ";
+cin >> nota1;
+cout << "Dame la segunda nota: ";
+cin >> nota2;
+cout << "Dame la tercera nota: ";
+cin >> nota3;
+cout << "Dame la cuarta nota: ";
+cin >> nota4;
+media= (nota1+nota1+nota3+nota4)/4;
+cout << "La media de la nota: " <<media <<endl;
+
+return 0;
+
+
+}
