415558167e665bdee1f79514d2110da16eac2fbd
Pablo080 <pablo.vela.castillo@gmail.com>
2022-03-09 10:46:41 +0100

    decimales

---
+++ decimales.cpp
@@ -0,0 +1,14 @@
+//Decimales
+#include <iostream>
+using namespace std;
+int main ()
+{
+float n1, n2, cociente;
+cout<<"Dime un numero:";
+cin>>n1;
+cout<<"Dime otro numero:";
+cin>>n2;
+cociente = n1/n2;
+cout <<"La división es"<<cociente <<endl;
+return 0;
+}
