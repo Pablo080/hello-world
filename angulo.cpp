41bce9b462eb10b5d6123ba737c39f92224f8608
Pablo080 <pablo.vela.castillo@gmail.com>
2022-03-10 10:56:00 +0100

    angulo

---
+++ angulo.cpp
@@ -0,0 +1,12 @@
+//Cuadrado de un número
+#include <iostream>
+int main ()
+{
+int 8; 
+std::cout<<”Dime un numero:”; Pedimos el número al usuario
+std::cin>>numero;
+std:: cout <<”El cuadrado de tu numero es”<<numero*numero;
+Calculamos y mostramos el
+ resultado.
+return 0;
+}
