bad29840b873666e038647ff2a61d04577ac1571
Pablo080 <99122105+Pablo080@users.noreply.github.com>
2022-05-12 11:05:30 +0200

    Examen-Pablo Vela

---
+++ examen...cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+
+int aleatorio(){
+	int num;
+	srand(time(NULL));
+	num=rand()%11;
+	return num;
+}
+
+int main() {
+	int numIntentos;
+	int numAciertos;
+	int userNum;
+	int numAleatorio;
+	int numero;
+	
+	
+	cout << "El juego consiste en acertar un numero aleatorio del uno al diez las veces que quieras. ";
+    do
+    {
+    cout << "Escribe un numero positivo ";
+    cin >> numero;
+    if (numero <=0)
+    cout << "Prueba otro numero" << endl;
+}
+{
+    while (numero > 0) 
+	cout << "El numero es positivo" ;
+}
+{
+	while (userNum = numAleatorio) cout >> "Ese es, sigue intentando o escribe un numero negativo para salir del bucle";
+	if  
+	
+	
+
+
+	while (userNum = numAleatorio) cout >> ""
+    cout << "Ese numero no es positivo" <<endl;
+    
+    
+    cout << "Escribe otro numero positivo";
+    cin >> numero
+	
+		
+	break;
+	
+ 
+	int contador = 0;
+	while (contador < 10){
+		contador++; 
+	} 
+	
+    do{
+    
+    
+
+}
+	
+	
+	
+	return 0;
+}
